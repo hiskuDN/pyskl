c74b9bd670c2230695853be9437a9f4b9e83412b
Hiskias <hiskias.melke1@gmail.com>
2024-10-02 17:16:58 +0900

    refactor: Update pipeline for stgcn_hisku_hmdb51_hrnet/j.py and j_cutout.py

--- configs/stgcn/stgcn_hisku_hmdb51_hrnet/j_cutout.py
+++ configs/stgcn/stgcn_hisku_hmdb51_hrnet/j_cutout.py
@@ -2,35 +2,18 @@ model = dict(
     type='RecognizerGCN',
     backbone=dict(
         type='STGCN',
+        tcn_dropout=0.5,
         graph_cfg=dict(layout='coco', mode='stgcn_spatial')),
     cls_head=dict(type='GCNHead', num_classes=51, in_channels=256))
 
 dataset_type = 'PoseDataset'
 ann_file = 'data/hmdb51/hmdb51_hrnet.pkl'
-train_pipeline = [
+pipeline = [
     dict(type='PreNormalize2D'),
     dict(type='GenSkeFeat', dataset='coco', feats=['j']),
-    dict(type='UniformSample', clip_len=100),
+    dict(type='PadTo', length=300, mode='zero'),
     dict(type='PoseDecode'),
     dict(type='RandomCutout', num_joints_to_cut=10),
-    dict(type='FormatGCNInput', num_person=2),
-    dict(type='Collect', keys=['keypoint', 'label'], meta_keys=[]),
-    dict(type='ToTensor', keys=['keypoint'])
-]
-val_pipeline = [
-    dict(type='PreNormalize2D'),
-    dict(type='GenSkeFeat', dataset='coco', feats=['j']),
-    dict(type='UniformSample', clip_len=100, num_clips=1),
-    dict(type='PoseDecode'),
-    dict(type='FormatGCNInput', num_person=2),
-    dict(type='Collect', keys=['keypoint', 'label'], meta_keys=[]),
-    dict(type='ToTensor', keys=['keypoint'])
-]
-test_pipeline = [
-    dict(type='PreNormalize2D'),
-    dict(type='GenSkeFeat', dataset='coco', feats=['j']),
-    dict(type='UniformSample', clip_len=100, num_clips=10),
-    dict(type='PoseDecode'),
     dict(type='FormatGCNInput', num_person=2),
     dict(type='Collect', keys=['keypoint', 'label'], meta_keys=[]),
     dict(type='ToTensor', keys=['keypoint'])
@@ -42,15 +25,15 @@ data = dict(
     train=dict(
         type='RepeatDataset',
         times=5,
-        dataset=dict(type=dataset_type, ann_file=ann_file, pipeline=train_pipeline, split='xview_train')),
-    val=dict(type=dataset_type, ann_file=ann_file, pipeline=val_pipeline, split='xview_val'),
-    test=dict(type=dataset_type, ann_file=ann_file, pipeline=test_pipeline, split='xview_val'))
+        dataset=dict(type=dataset_type, ann_file=ann_file, pipeline=pipeline, split='train1')),
+    val=dict(type=dataset_type, ann_file=ann_file, pipeline=pipeline, split='test1'))
+data['test'] = data['val']
 
 # optimizer
-optimizer = dict(type='SGD', lr=0.1, momentum=0.9, weight_decay=0.0005, nesterov=True)
+optimizer = dict(type='SGD', lr=0.1, momentum=0.9, weight_decay=0.0001)
 optimizer_config = dict(grad_clip=None)
 # learning policy
-lr_config = dict(policy='CosineAnnealing', min_lr=0, by_epoch=False)
+lr_config = dict(policy='step', step=[2, 10])
 total_epochs = 16
 checkpoint_config = dict(interval=1)
 evaluation = dict(interval=1, metrics=['top_k_accuracy'])
@@ -58,4 +41,4 @@ log_config = dict(interval=100, hooks=[dict(type='TextLoggerHook')])
 
 # runtime settings
 log_level = 'INFO'
-work_dir = './work_dirs/stgcn/stgcn_pyskl_ntu60_xview_hrnet/j'
+work_dir = './work_dirs/stgcn/stgcn_hisku_hmdb51_hrnet/j'
